18d4e1e1c3f6f09b180373a88a6fe161eb8a1da4
jimin Kim <gjimin26@gmail.com>
2025-07-28 22:04:14 +0900

    jwt 토큰에서 id 추가 및 프로필 나눔

--- src/main/java/com/swygbro/packup/security/jwt/JwtUtill.java
+++ src/main/java/com/swygbro/packup/security/jwt/JwtUtill.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -18,7 +19,7 @@ public class JwtUtill {
 
     private SecretKey key;
 
-    public JwtUtill(@Value("${spring.jwt.secret}") String secret) {
+    public JwtUtill(@Value("${JWT_SECRET_KEY}") String secret) {
         key = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
@@ -33,27 +34,32 @@ public class JwtUtill {
         return Jwts.parser().verifyWith(key).build().parseSignedClaims(jws).getPayload().get("role", String.class);
     }
 
+    // jwt bearer 토큰으로부터 userId을 추출
+    public  String getUserId(String jws) {
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(jws).getPayload().get("userId", String.class);
+    }
+
     // jwt bearer 토큰이 만료 되었는지 확인
     public Boolean isExpired(String jws) {
         System.out.println(Jwts.parser().verifyWith(key).build().parseSignedClaims(jws).getPayload().getExpiration());
         return Jwts.parser().verifyWith(key).build().parseSignedClaims(jws).getPayload().getExpiration().before(new Date());
     }
 
-    // jwt 토큰 생성
-
     /**
-     *
+     * jwt 토큰 생성
      * @param username, role: payload에 넣을 사용자 정보
      * @param expiredMs: 만료 시간(밀리초 단위)
      * @return
      */
-    public String createToken(String username, String role, Long expiredMs) {
+    public String createToken(String username, String role, String userId, Long expiredMs) {
         return Jwts.builder()
                 .claim("username", username)
                 .claim("role", role)
+                .claim("userId", userId)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(key)
                 .compact();
     }
+
 }
